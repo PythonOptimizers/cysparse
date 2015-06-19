542ebfa59db809c4a93f1dfcd00f1b4ce95e03b1
counterclocker <nikolaj.van.omme@gmail.com>
2015-06-19 18:15:03 -0400

    .H: four cases of multiplication for LLSparseMatrix quickly tested

--- examples/conjugate_transposed.py
+++ examples/conjugate_transposed.py
@@ -42,3 +42,35 @@ H2 = l2.H
 print H2
 
 print H2.matvec(b)
+
+
+########################################################################################################################
+print "=" * 80
+
+l3 = NewLLSparseMatrix(nrow=2, ncol=3, size_hint=10, dtype=COMPLEX256_T)
+l3.put_triplet([1, 1, 1], [0, 1, 2], [1+1j, 1-1.0j, 2+1j])  # i, j, val
+
+l3.print_to(sys.stdout)
+
+H3 = l3.H
+
+print H3
+
+c = np.array([1+1j,0, 1+1j, 0], dtype=np.complex256)
+
+print H3.matvec(c[::2])
+
+########################################################################################################################
+print "=" * 80
+
+l4 = NewLLSparseMatrix(nrow=2, ncol=3, size_hint=10, dtype=COMPLEX256_T, is_symmetric=True)
+l4.put_triplet([1, 1], [0, 1], [1+1j, 1-1.0j])  # i, j, val
+
+l4.print_to(sys.stdout)
+
+H4 = l4.H
+
+print H4
+
+print H4.matvec(c[::2])
+
