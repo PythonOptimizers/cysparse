4458c657f5629a82e04f35a233cb871d827f084a
counterclocker <nikolaj.van.omme@gmail.com>
2015-07-07 13:29:15 -0400

    added delete_cols to LLSparseMatrix

---
+++ examples/delete_col.py
@@ -0,0 +1,16 @@
+from cysparse.sparse.ll_mat import *
+import cysparse.types.cysparse_types as types
+import numpy as np
+
+import sys
+
+l1 = NewLinearFillLLSparseMatrix(ncol=10, nrow=10)
+
+########################################################################################################################
+print "=" * 80
+
+print l1
+
+l1.delete_cols([2, 3, 0])
+
+print l1
