1709125785ca522d9405c2d670ced15bddd5ffd6
counterclocker <nikolaj.van.omme@gmail.com>
2015-07-19 11:26:37 -0400

    tried several options to absolute/relative import for tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,21 @@
+import unittest
 
-import unittest2
-loader = unittest2.TestLoader()
-tests = loader.discover('.')
-testRunner = unittest2.runner.TextTestRunner()
-testRunner.run(tests)
+testmodules = [
+    'cysparse.linalg.suitesparse.umfpack.generic.test_umfpack',
+    'cysparse.test_whiteutils',
+    'cysparse.test_cogapp',
+    ]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+    try:
+        # If the module defines a suite() function, call it to get the suite.
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        # else, just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
