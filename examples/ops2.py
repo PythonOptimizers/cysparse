cebba22c93c8202bed1365f58602cca98699b5b6
counterclocker <nikolaj.van.omme@gmail.com>
2016-01-10 11:39:59 -0500

    add example with A.T and sum operator

--- examples/ops2.py
+++ examples/ops2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 A = LinearFillLLSparseMatrix(nrow=4, ncol=3, dtype=COMPLEX128_T, first_element=1-0.2j)
 B = LinearFillLLSparseMatrix(nrow=3, ncol=4, dtype=COMPLEX128_T)
-C = LinearFillLLSparseMatrix(size=4, dtype=COMPLEX128_T)
+C = LinearFillLLSparseMatrix(size=4, dtype=COMPLEX128_T, store_symmetric=True)
 
 print A
 print B
