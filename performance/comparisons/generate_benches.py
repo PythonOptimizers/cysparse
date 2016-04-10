15549e9e109469782b82c889e3fbe753094f9a46
counterclocker <nikolaj.van.omme@gmail.com>
2016-04-10 19:39:45 +0300

    added script to generate tables in article

--- performance/comparisons/generate_benches.py
+++ performance/comparisons/generate_benches.py
@@ -17,7 +17,7 @@ def generate_txt_benches(sub_dir, suffix='bench.txt'):
     python_script_benches = glob.glob(os.path.join(sub_dir, '*.py'))
 
     for script in python_script_benches:
-        if script in ['__init__.py', 'generate_benches.py']:
+        if os.path.basename(script) in ['__init__.py', 'generate_benches.py', 'generate_tables.py']:
             break
         print script
         subprocess.call("python %s > %s" % (script, script[:-3] + '.' + suffix), shell=True)
